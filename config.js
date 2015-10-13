e68d4105902f348e0dffeea731f5e114147b5a03
Travis Benton <tbenton@spredfast.com>
2015-10-12 21:56:44 -0500

    add config back

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {
+  email: {
+    SERVICE : '',
+    USER    : '',
+    PASS    : ''
+  },
+  twitter: {
+    HANDLE              : '',
+    CONSUMER_KEY        : '',
+    CONSUMER_SECRET     : '',
+    ACCESS_TOKEN_KEY    : '',
+    ACCESS_TOKEN_SECRET : ''
+  }
+};
+
+module.exports = config;
